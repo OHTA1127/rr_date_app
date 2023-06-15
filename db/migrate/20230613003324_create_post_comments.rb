a845cc8406911883691a03ddfce358208f5e0c99
小野桜汰 <ohta@outa.local>
2023-06-15 11:45:35 +0900

    favoritesテーブルの削除

--- db/migrate/20230613003324_create_post_comments.rb
+++ db/migrate/20230613003324_create_post_comments.rb
@@ -2,8 +2,8 @@ class CreatePostComments < ActiveRecord::Migration[7.0]
   def change
     create_table :post_comments do |t|
       t.text :comment
-      t.references :user, null: false, foreign_key: true
-      t.references :post, null: false, foreign_key: true
+      t.references :user, foreign_key: true
+      t.references :post, foreign_key: true
 
       t.timestamps
     end
